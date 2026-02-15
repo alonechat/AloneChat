39ed2058fd95cac67b2da468822dcef85daf83bf
张小云 <160196654+hi-zcy@users.noreply.github.com>
2026-02-15 20:29:32 +0800

    chore: Add a ascii art and update version number

--- AloneChat/__init__.py
+++ AloneChat/__init__.py
@@ -1,1 +1,17 @@
-__version__ = "1.5.0"
+"""
+    ___    __                 ________          __
+   /   |  / /___  ____  ___  / ____/ /_  ____ _/ /_
+  / /| | / / __ \/ __ \/ _ \/ /   / __ \/ __ `/ __/
+ / ___ |/ / /_/ / / / /  __/ /___/ / / / /_/ / /_
+/_/  |_/_/\____/_/ /_/\___/\____/_/ /_/\__,_/\__/
+
+AloneChat Project - A modular chat application framework.
+
+Originally developed by: @hi-zcy, @hi-tyc (@tony231218)
+Developed by: AloneChat Team, and open-source community of contributors.
+License: Apache-2.0 License (Dependencies' licenses stored in NOTICE file)
+
+"Let chat no longer be alone."
+"""
+
+__version__ = "2.0.0-Beta"
